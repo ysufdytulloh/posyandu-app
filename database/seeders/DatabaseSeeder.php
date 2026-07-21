4555ddb37ca77ce17e347c1f6c62b1b02a3e3653
ysufhidayatulloh <ysfhidayatulloh12@gmail.com>
2026-07-21 13:16:01 +0700

    chore: update DatabaseSeeder - semua seeder jadi satu perintah

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,9 +9,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            ZscoreReferensiSeeder::class,
             JenisImunisasiSeeder::class,
             JenisPmtSeeder::class,
-            ZScoreReferensiSeeder::class,
+            DatabaseCompleteSeeder::class,
         ]);
     }
 }
